cafc7f0736231e385f4052b737dc37ea1480b66a
Luis Gomez <whoislewys@gmail.com>
2020-06-24 09:26:28 -0700

    tostring for Note

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,12 +1,22 @@
 class Note {
   final String note;
+  final int audioId;
 
-  Note({this.note});
+  Note({this.note, this.audioId});
 
   Map<String, dynamic> toMap() {
     return {
       'note': note,
+      'audio_id': audioId,
     };
   }
 
+  @override
+  String toString() {
+    return '''
+
+        Note
+        note: ${this.note}
+        audio_id: ${this.audioId}''';
+  }
 }

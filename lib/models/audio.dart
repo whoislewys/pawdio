18249111f92c39c6ad90c4fa2a8e97156f3b2262
whoislewys <whoislewys@gmail.com>
2020-09-21 20:25:02 -0600

    update deps + add audio model

---
+++ lib/models/audio.dart
@@ -0,0 +1,26 @@
+class Audio {
+  final int filePath;
+  final int lastPosition;
+
+  Audio({this.filePath, this.lastPosition});
+
+  factory Audio.fromRow(Map<String, dynamic> row) {
+    return Audio(filePath: row['file_path'], lastPosition: row['last_position']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'file_path': filePath,
+      'last_position': lastPosition,
+    };
+  }
+
+  @override
+  String toString() {
+    return '''
+
+        Audio
+        filePath: ${this.filePath}
+        last_position: ${this.lastPosition}''';
+  }
+}
